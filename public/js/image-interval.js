0f782dcd618e21cc93b377d355741ed8eae337a9
Seth <sz16900@my.bristol.ac.uk>
2017-05-25 19:32:53 +0100

    Image Flipper. Extra content.

---
+++ public/js/image-interval.js
@@ -0,0 +1,21 @@
+var imageId = 0;
+
+function flipImage() {
+
+  var image1 = document.getElementById("pic1");
+  var image2 = document.getElementById("pic2");
+
+  //flick depending on mod can be added for more pics in case of a longer conversation
+  if(imageId %2 == 0){
+    image1.style.display = 'block';
+    image2.style.display = 'none';
+  }
+  else {
+    image2.style.display = 'block';
+    image1.style.display = 'none';
+  }
+  imageId = imageId + 1;
+}
+
+//call same function again for x ammount of seconds
+setInterval(flipImage, 1000);
